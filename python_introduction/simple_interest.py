fa4a3e2fe79d3f7142e69b48648fb2f91a002fd1
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-09-13 12:41:24 +0100

    Add calc for S.I in simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000
+rate = 0.05
+time = 3
+
+#Calculate the simple interest using the formula provided and store the result in a variable named interest.
+interest = principal * rate * time
+
+print(f"The simple interest is: {float(interest)}")
