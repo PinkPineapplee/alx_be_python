3b6a5442799170f7bcbd41755cda85f5c637ee99
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-09-18 15:45:46 +0100

    /usr/local/bin/python3 /Users/admin/alx/alx_be_python/control-flow/match_case_calculator.py Add divide by zero functionality

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation(+, -, *, /): ")
+result = 0
+
+match operator:
+ case "+":
+  result = num1 + num2
+  print("The result is ", result)
+ case "-":
+  result = num1 - num2
+  print("The result is ", result)
+ case "*":
+  result = num1 * num2
+  print("The result is ", result)
+ case "/":
+  result = num1 / num2
+  print("The result is", result)
+ case _:
+  print("Invalid")
+
+  
