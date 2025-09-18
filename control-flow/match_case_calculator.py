831363d7b59f84e628d5f80f0d0858a6907c65d5
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-09-18 18:45:40 +0100

    Add space to operation input

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,9 +1,8 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operator = input("Choose the operation(+, -, *, /): ")
-result = 0
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operator:
+match operation:
  case "+":
   result = num1 + num2
   print(f"The result is {result}.")
