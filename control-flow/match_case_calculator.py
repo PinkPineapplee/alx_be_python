1a71054d4668101eaaccb3cab441516c10f047ed
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-09-18 16:06:17 +0100

    update divide by zero error handling

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -14,10 +14,11 @@ match operator:
   result = num1 * num2
   print("The result is ", result)
  case "/":
-  result = num1 / num2
-  print("The result is", result)
+  if (num1 == 0 or num2 == 0):
+    print("Cannot divide by zero")
+  else:
+    result = num1 / num2
+    print("The result is", result)
  case _:
   print("Invalid")
 
-if (num1 == 0 or num2 == 0) and (operator == "/"):
-   print("Cannot divide by zero")
