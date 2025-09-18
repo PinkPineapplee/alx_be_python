58eabeb81a38df392a96dae5d4d73d4695bf35ed
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-09-18 16:14:36 +0100

    change print syntax

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -6,19 +6,19 @@ result = 0
 match operator:
  case "+":
   result = num1 + num2
-  print("The result is ", result)
+  print(f"The result is {result}.")
  case "-":
   result = num1 - num2
-  print("The result is ", result)
+  print(f"The result is {result}.")
  case "*":
   result = num1 * num2
-  print("The result is ", result)
+  print(f"The result is {result}.")
  case "/":
   if (num1 == 0 or num2 == 0):
     print("Cannot divide by zero")
   else:
     result = num1 / num2
-    print("The result is", result)
+    print(f"The result is {result}.")
  case _:
   print("Invalid")
 
