a5c85ea4b46b765d65f258bb83f40f68eb50d1ec
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-10-06 10:45:31 +0100

    Add oop dir & files

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount ():
+    def __init__(self,account_balance):
+    self.account_balance= account_balace
+
+    def deposit(amount):
+       return account_balance += amount
+
+    def withdraw(amount):
+       return account_balance -= amount
+
+    def display_balance(amount): 
+       if funds       
