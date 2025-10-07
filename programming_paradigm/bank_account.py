bbc69eb80bcd53b4524295d1453e069f9f23120b
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-10-07 07:21:34 +0100

    add database to save deposit and withdrawals

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-     self.account_balance = initial_balance
-
+     self.account_balance = initial_balance 
+     
     def deposit(self, amount):
        self.account_balance += amount
 
@@ -9,7 +9,7 @@ class BankAccount:
       if self.account_balance < amount:
           return False
       else:
-          self.account_balace -= amount
+          self.account_balance -= amount
           return True
 
     def display_balance(self): 
