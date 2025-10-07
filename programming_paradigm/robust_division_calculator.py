b096627c832f6cd3abf264705770d49a0d76caf8
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-10-07 09:32:23 +0100

    Add whitespace to front

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,8 +3,8 @@ def safe_divide(numerator, denominator):
         num = float(numerator)
         denom = float(denominator)
         result = num / denom
-        print(f"The result of the division is {result} ")
+        print(f" The result of the division is {result}")
     except ValueError:
-        print("Error: Please enter numeric values only. ")
+        print(" Error: Please enter numeric values only.")
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero. ")
+        print(" Error: Cannot divide by zero.")
