c82b20ad665a83479cc589968fd1f922b3444be5
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-10-07 13:16:54 +0100

    fix syntax bug

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,7 +17,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
 
-    def test_multiplicaton(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
         self.assertEqual(self.calc.multiply(-2, -2), 4)
