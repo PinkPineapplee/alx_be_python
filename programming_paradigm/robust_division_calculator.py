b82ed8344c716c54020cecfde98f37c694384471
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-10-07 09:50:32 +0100

    Add return

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,8 +3,11 @@ def safe_divide(numerator, denominator):
         num = float(numerator)
         denom = float(denominator)
         result = num / denom
-        print(f" The result of the division is {result}")
+        print(f"The result of the division is {result}")
+        return
     except ValueError:
-        print(" Error: Please enter numeric values only.")
+        print("Error: Please enter numeric values only.")
+        return
     except ZeroDivisionError:
-        print(" Error: Cannot divide by zero.")
+        print("Error: Cannot divide by zero.")
+        return
